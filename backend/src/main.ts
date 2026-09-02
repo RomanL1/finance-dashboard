87b318cd369f604adb5b11268ee9776d0784e4f6
RomanL1 <40143010+RomanL1@users.noreply.github.com>
2026-09-02 08:52:21 +0200

    refactor: use express platfrom

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module.js';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('finance-dashboard-api')
     .setVersion('1.0')
